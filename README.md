9655078719b6d9171c49531d1fb6ec307536811c
SARA SIMON <smbsimon@gmail.com>
2015-10-19 13:15:33 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#VPRJAMS
+
+This is a super quick little app (based off of the VPR Template) to aggregate tweets and instagrams from the 2015 Vermont Tech Jam. Thank you, iframes.
---
+++ main/views.py
@@ -0,0 +1,27 @@
+from main import app
+from flask import render_template, request
+from config import FREEZER_BASE_URL
+
+
+@app.route('/')
+def index():
+    page_title = '#VPRJAMS'
+    page_subtitle = 'from Vermont Public Radio'
+    page_url = FREEZER_BASE_URL.rstrip('/') + request.path
+
+
+    social = {
+        'title': "",
+        'subtitle': "",
+        'img': "",
+        'description': "",
+        'twitter_text': "",
+        'creator': "",
+        'twitter_hashtag': ""
+    }
+
+    return render_template('content.html',
+        page_title=page_title,
+        page_subtitle=page_subtitle,
+        page_url=page_url,
+        social=social)
